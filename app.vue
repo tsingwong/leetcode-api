e0f5a0ae19d6cb9c838db41b63f7227ff98969f6
tsingwong <919916459@qq.com>
2022-05-15 14:11:17 +0800

    2 files modified, 6 added, 3 deleted

--- app.vue
+++ app.vue
@@ -1,16 +1,8 @@
 <script setup>
-  const version = 2 + 1
+const version = 2 + 1;
 </script>
 <template>
-  <div>
-    Hello Nuxt {{ version }}
-    <a-button>Here we are</a-button>
-    <a-button type="primary">Primary Button</a-button>
-
-    <nav class="flex align-center gap-4 p-4">
-      <NuxtLink to="/default"> Default layout </NuxtLink>
-    </nav>
-
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,7 @@
 <template>
   <nav class="flex align-center gap-4 p-4">
     <NuxtLink to="/default"> Default layout </NuxtLink>
+    <NuxtLink to="/custom"> Custom layout </NuxtLink>
+    <NuxtLink to="/dynamic"> Dynamic layout </NuxtLink>
   </nav>
 </template>
